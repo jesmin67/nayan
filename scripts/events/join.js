504aff568e1bdfffa297bb0b736a34a0718a8add
X-Jesmin <prtuber70@gmail.com>
2024-09-25 10:12:42 +0800

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -56,7 +56,7 @@ axios.get(gifUrl, { responseType: 'arraybuffer' })
     fs.writeFileSync(gifPath, response.data);
   if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
     api.changeNickname(`[ ${global.config.PREFIX} ] • ➠${(!global.config.BOTNAME) ? "bot" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
-    return api.sendMessage("চলে এসেছি আমি পিচ্চি নয়ন তোমাদের মাঝে🤭!", event.threadID, () => api.sendMessage({ body: `${global.config.BOTNAME} CONNECTED«\n\nAssalamualaykum☘️
+    return api.sendMessage("চলে এসেছি আমি EMon-BHai-Bot তোমাদের মাঝে🤭!", event.threadID, () => api.sendMessage({ body: `${global.config.BOTNAME} CONNECTED«\n\nAssalamualaykum☘️
 <------------------------------>  
 BOT CONNECTED SUCCESFUL !!! 
 
@@ -66,6 +66,16 @@ APPROVAL ALLOW IN THIS GROUP!!!
 <------------------------------>
 AND FOR ANY COMPLAINTS OR CONTACT BOT OPERATOR 
 
+ADMIN BOT : EMON HAWLADAR ( EMon-BHai )
+
+🟣Facebook Account Link: 
+
+https://www.facebook.com/EMon.BHai.FACEBOOK
+
+🔵WHATSAPP NUMBER: wa.me/+8801309991724
+
+🟢SUPPORT EMAIL: emonhawladar311@gmail.com
+
 DEVELOPER :Mohammad Nayan 
 
 🟣Facebook Account Link: 
@@ -74,7 +84,7 @@ https://www.facebook.com/www.xnxx.com169
 
 🔵WHATSAPP NUMBER: wa.me/+8801615298449
 
-🟢SUPPORT EMAIL: www.mdmnnm2004@gmail.com`, attachment: fs.createReadStream(gifPath)}, threadID));
+🟢SUPPORT EMAIL: mdmnnm2004@gmail.com`, attachment: fs.createReadStream(gifPath)}, threadID));
   }})
 .catch(error => {
     console.error(error);
